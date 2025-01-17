364aec7fb407ff550645f41b4992dfae25bdf147
Shipa Shipovnik <shipovnik2020d@gmail.com>
2025-01-18 01:40:31 +0300

    Подготовка бекенда и фронта к работе с апи

--- server/services/urls.py
+++ server/services/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # улсуги
     path('services-list/', ServicesList.as_view(), name='services-list'),
-    path('categories-list/', CategoriesList.as_view(), name='categories-list'),
     path('<int:pk>/', ServiceDetailView.as_view(), name='service-detail'),
     path('create/', ServiceCreateView.as_view(), name= 'add-service'),
+    # категории
+    path('categories-list/', CategoriesList.as_view(), name='categories-list'),
     path('category/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
-    path('create/', CategoryCreateView.as_view(), name='add-category'),
+    path('category/create/', CategoryCreateView.as_view(), name='add-category'),
 ]
--- server/users/urls.py
+++ server/users/urls.py
@@ -4,6 +4,7 @@ from users.views import *
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
+    # логин нужно
     path('profile/', UserProfileView.as_view(), name='user-profile'),
     path('profile/<int:profile_id>/', ProfileDetailView.as_view(), name='profile-details'),
     path('users-list/', UsersList.as_view(), name='users-list'),
